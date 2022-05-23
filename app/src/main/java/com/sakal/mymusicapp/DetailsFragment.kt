b1e274e3e5da3e17328dbb0cd973d13da6f3f3c8
NadezhdaSakal <79761587+NadezhdaSakal@users.noreply.github.com>
2022-05-23 19:16:55 +0300

    add search&playlists

--- app/src/main/java/com/sakal/mymusicapp/DetailsFragment.kt
+++ app/src/main/java/com/sakal/mymusicapp/DetailsFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_details.*
+import android.content.Intent
+
 
 class DetailsFragment : Fragment() {
 
