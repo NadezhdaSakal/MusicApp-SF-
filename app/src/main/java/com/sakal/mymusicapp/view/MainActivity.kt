65e1bbf0d381fd72b0d4b1ddecc05351955bbeb9
NadezhdaSakal <79761587+NadezhdaSakal@users.noreply.github.com>
2022-05-26 23:52:17 +0300

    dagger ok  api

--- app/src/main/java/com/sakal/mymusicapp/view/MainActivity.kt
+++ app/src/main/java/com/sakal/mymusicapp/view/MainActivity.kt
@@ -1,22 +1,25 @@
 package com.sakal.mymusicapp.view
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.sakal.mymusicapp.R
+import androidx.fragment.app.Fragment
+import com.sakal.mymusicapp.*
+import com.sakal.mymusicapp.databinding.ActivityMainBinding
 import com.sakal.mymusicapp.domain.Audio
-import com.sakal.mymusicapp.view.fragments.DetailsFragment
-import com.sakal.mymusicapp.view.fragments.HomeFragment
-import kotlinx.android.synthetic.main.activity_main.*
+import com.sakal.mymusicapp.view.fragments.*
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
 
         initNavigation()
-
         supportFragmentManager
             .beginTransaction()
             .add(R.id.fragment_placeholder, HomeFragment())
@@ -27,6 +30,7 @@ class MainActivity : AppCompatActivity() {
 
     fun launchDetailsFragment(audio: Audio) {
         val bundle = Bundle()
+
         bundle.putParcelable("audio", audio)
         val fragment = DetailsFragment()
         fragment.arguments = bundle
@@ -39,46 +43,46 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initNavigation() {
-        topAppBar.setOnMenuItemClickListener {
-
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
             when (it.itemId) {
+                androidx.appcompat.R.id.home -> {
+                    val tag = "home"
+                    val fragment = checkFragmentExistence(tag)
+
+
+                    changeFragment( fragment?: HomeFragment(), tag)
+                    true
+                }
                 R.id.favorites -> {
-                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                    val tag = "favorites"
+                    val fragment = checkFragmentExistence(tag)
+                    changeFragment( fragment?: FavoritesFragment(), tag)
                     true
                 }
                 R.id._latest -> {
-                    Toast.makeText(this, "Поcледнее", Toast.LENGTH_SHORT).show()
+                    val tag = "latest"
+                    val fragment = checkFragmentExistence(tag)
+                    changeFragment( fragment?: LatestFragment(), tag)
                     true
                 }
                 R.id.playlist -> {
-                    Toast.makeText(this, "Плэйлисты", Toast.LENGTH_SHORT).show()
-                    true
-                }
-                else -> false
-            }
-        }
-
-        bottom_navigation.setOnNavigationItemSelectedListener {
-
-            when (it.itemId) {
-                R.id.play -> {
-                    Toast.makeText(this, "Play", Toast.LENGTH_SHORT).show()
-                    true
-                }
-                R.id.next -> {
-                    Toast.makeText(this, "Next audio", Toast.LENGTH_SHORT).show()
-                    true
-                }
-                R.id.playback_order -> {
-                    Toast.makeText(this, "Shuffle", Toast.LENGTH_SHORT).show()
+                    val tag = "selections"
+                    val fragment = checkFragmentExistence(tag)
+                    changeFragment( fragment?: PlaylistsFragment(), tag)
                     true
                 }
                 else -> false
             }
         }
     }
+
+    private fun checkFragmentExistence(tag: String): Fragment? = supportFragmentManager.findFragmentByTag(tag)
+
+    private fun changeFragment(fragment: Fragment, tag: String) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_placeholder, fragment, tag)
+            .addToBackStack(null)
+            .commit()
+    }
 }
-
-
-
-
