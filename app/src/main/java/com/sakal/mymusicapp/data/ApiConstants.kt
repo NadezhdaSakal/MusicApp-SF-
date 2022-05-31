f974d3025db34e3818c7ddf1bb0858755e7d10eb
NadezhdaSakal <79761587+NadezhdaSakal@users.noreply.github.com>
2022-05-31 15:13:39 +0300

    dagger ok  api

--- app/src/main/java/com/sakal/mymusicapp/data/ApiConstants.kt
+++ app/src/main/java/com/sakal/mymusicapp/data/ApiConstants.kt
@@ -2,7 +2,6 @@ package com.sakal.mymusicapp.data
 
 object ApiConstants {
     const val BASE_URL = "http://phish.in/api/v1/"
-    const val PlAYLISTS_URL ="https://api.jamendo.com/v3.0/playlists/"
     const val IMAGES_URL = ""
 
 }
