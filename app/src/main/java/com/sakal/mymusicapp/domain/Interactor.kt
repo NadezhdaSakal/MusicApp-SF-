952efcea8a105011c5e7880c9d18be7685adf5ef
NadezhdaSakal <79761587+NadezhdaSakal@users.noreply.github.com>
2022-06-05 01:12:18 +0300

    TopTracks

--- app/src/main/java/com/sakal/mymusicapp/domain/Interactor.kt
+++ app/src/main/java/com/sakal/mymusicapp/domain/Interactor.kt
@@ -10,25 +10,9 @@ import retrofit2.Response
 
 class Interactor(private val repo: MainRepository, private val retrofitService: LastFMApi) {
 
-   /* fun getArtistsFromApi(page: Int, callback: HomeFragmentViewModel.ApiCallback) {
-        retrofitService.getTopArtists(limit = 50, page).enqueue(object : Callback<ArtistsWrapper> {
-            override fun onResponse(
-                call: Call<ArtistsWrapper>,
-                response: Response<ArtistsWrapper>
-            ) {
-                callback.onSuccess(Converter.convertApiListToDTOList(response.body()?.artists?.artist))
-            }
-
-            override fun onFailure(call: Call<ArtistsWrapper>, t: Throwable) {
-                callback.onFailure()
-            }
-        })
-    }
-*/
     fun getTracksFromApi(page: Int, callback: HomeFragmentViewModel.ApiCallback) {
-        retrofitService.getTracks(limit =50, page).enqueue(object : Callback<TracksWrapper> {
-            override fun onResponse
-                        (call: Call<TracksWrapper>, response: Response<TracksWrapper>) {
+        retrofitService.getTracks(limit = 50, page).enqueue(object : Callback<TracksWrapper> {
+            override fun onResponse(call: Call<TracksWrapper>, response: Response<TracksWrapper>) {
                 callback.onSuccess(Converter.convertApiTrackListToDTOList(response.body()?.tracks?.track))
             }
 
@@ -37,8 +21,6 @@ class Interactor(private val repo: MainRepository, private val retrofitService: 
             }
         })
     }
-
-
 }
 
 
