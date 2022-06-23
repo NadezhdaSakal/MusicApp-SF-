4ca6224172be130efd702a894d4b4eafc52c9a1b
NadezhdaSakal <79761587+NadezhdaSakal@users.noreply.github.com>
2022-06-23 14:20:40 +0300

    pagination

--- app/src/main/java/com/sakal/mymusicapp/TracksDataSource.kt
+++ app/src/main/java/com/sakal/mymusicapp/TracksDataSource.kt
@@ -5,25 +5,28 @@ import androidx.paging.PagingState
 import com.sakal.mymusicapp.data.LastFMApi
 import com.sakal.mymusicapp.data.entity.*
 import retrofit2.Call
+import retrofit2.Response
+import retrofit2.awaitResponse
 
 class TracksDataSource(private val api: LastFMApi) : PagingSource<Int, Track>() {
 
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Track> {
-        return try {
-            val nextPageNumber = params.key ?: 0
-            val response: Call<TracksWrapper> = api.getTracks(limit = 100, page =1)
+        override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Track> {
+            return try {
+                val nextPageNumber = params.key ?: 0
+                val query: Call<TracksWrapper> = api.getTracks(50, 1)
+                val response: Response<TracksWrapper> = query.awaitResponse()
 
-            LoadResult.Page(
-                data = response.body()?.tracks?.track,
-                prevKey = if (nextPageNumber > 0) nextPageNumber - 1 else null,
-                nextKey = if (nextPageNumber < response.totalPages) nextPageNumber + 1 else null
-            )
-        } catch (e: Exception) {
-            LoadResult.Error(e)
+                LoadResult.Page(
+                    data = response.body()?.tracks!!.track,
+                    prevKey = if (nextPageNumber > 0) nextPageNumber - 1 else null,
+                    nextKey = if (nextPageNumber < response.body()?.tracks?.attr?.totalPages!!.toInt()) nextPageNumber + 1 else null
+                )
+            } catch (e: Exception) {
+                LoadResult.Error(e)
+            }
+        }
+
+        override fun getRefreshKey(state: PagingState<Int, Track>): Int? {
+            TODO("Not yet implemented")
         }
     }
-
-    override fun getRefreshKey(state: PagingState<Int, Track>): Int? {
-        TODO("Not yet implemented")
-    }
-}
