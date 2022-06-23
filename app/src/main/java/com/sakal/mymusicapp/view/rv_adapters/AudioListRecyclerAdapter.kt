2437742a57e93719cb9d89f5c75a4746d8b4e49d
NadezhdaSakal <79761587+NadezhdaSakal@users.noreply.github.com>
2022-06-23 17:25:58 +0300

    pagination

--- app/src/main/java/com/sakal/mymusicapp/view/rv_adapters/AudioListRecyclerAdapter.kt
+++ app/src/main/java/com/sakal/mymusicapp/view/rv_adapters/AudioListRecyclerAdapter.kt
@@ -1,9 +1,12 @@
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.sakal.mymusicapp.R
+import com.sakal.mymusicapp.data.entity.Audio
 import com.sakal.mymusicapp.data.entity.Image
 import com.sakal.mymusicapp.data.entity.Track
 import com.sakal.mymusicapp.databinding.AudioItemBinding
@@ -12,13 +15,10 @@ import kotlinx.android.synthetic.main.audio_item.view.*
 
 class AudioListRecyclerAdapter : PagingDataAdapter<Track, AudioViewHolder>(TrackDiffCallBack()) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AudioViewHolder {
-        return AudioViewHolder(
-            AudioItemBinding.inflate(
-                LayoutInflater.from(parent.context), parent, false
-            )
-        )
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return AudioViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.audio_item, parent, false))
     }
+
 
     override fun onBindViewHolder(holder: AudioViewHolder, position: Int) {
         holder.bind(getItem(position)?.image)
@@ -35,12 +35,12 @@ class TrackDiffCallBack : DiffUtil.ItemCallback<Track>() {
     }
 }
 
-class AudioViewHolder(itemView: AudioItemBinding) : RecyclerView.ViewHolder(itemView) {
+class AudioViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val track = itemView.track
     private val ava = itemView.ava
     private val artist = itemView.artist
 
-    fun bind(audio: List<Image>?) {
+    fun bind(audio: Audio) {
         track.text = audio.track
         Glide.with(itemView)
             .load(audio.image)
@@ -50,3 +50,4 @@ class AudioViewHolder(itemView: AudioItemBinding) : RecyclerView.ViewHolder(item
 
     }
 }
+
