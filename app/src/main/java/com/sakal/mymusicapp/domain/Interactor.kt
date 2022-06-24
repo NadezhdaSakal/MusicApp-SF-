6b977b2a5cd89c19600355b12988a2de275ac893
NadezhdaSakal <79761587+NadezhdaSakal@users.noreply.github.com>
2022-06-24 15:20:46 +0300

    pagination

--- app/src/main/java/com/sakal/mymusicapp/domain/Interactor.kt
+++ app/src/main/java/com/sakal/mymusicapp/domain/Interactor.kt
@@ -7,36 +7,18 @@ import com.sakal.mymusicapp.data.entity.Audio
 import com.sakal.mymusicapp.data.entity.Track
 import com.sakal.mymusicapp.data.entity.TracksWrapper
 import com.sakal.mymusicapp.utils.Converter
-import com.sakal.mymusicapp.viewmodel.HomeFragmentViewModel
-import com.sakal.mymusicapp.viewmodel.TracksViewModel
 import retrofit2.*
-import java.io.IOException
 
-class Interactor(private val repo: MainRepository, private val retrofitService: LastFMApi) {
-
-    fun getTracksFromApi(page: Int, callback: HomeFragmentViewModel.ApiCallback) {
-        retrofitService.getTracks(limit = 50, page).enqueue(object : Callback<TracksWrapper> {
-
-            override fun onResponse(call: Call<TracksWrapper>, response: Response<TracksWrapper>) {
-                val list = Converter.convertApiTrackListToDTOList(response.body()?.tracks?.track)
-                    repo.putToDb(list)
-                    callback.onSuccess(list)
-
-            }
-
-                override fun onFailure(call: Call<TracksWrapper>, t: Throwable) {
-                    callback.onFailure()
-                }
-            })
-        }
-
-    class TracksDataSource(private val api: LastFMApi) : PagingSource<Int, Track>() {
+class Interactor (private val repo: MainRepository, private val api: LastFMApi) : PagingSource<Int, Track>() {
 
         override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Track> {
             return try {
                 val nextPageNumber = params.key ?: 0
                 val query: Call<TracksWrapper> = api.getTracks(50, 1)
                 val response: Response<TracksWrapper> = query.awaitResponse()
+                val list = Converter.convertApiTrackListToDTOList(response.body()?.tracks?.track)
+                repo.putToDb(list)
+
 
                 LoadResult.Page(
                     data = response.body()?.tracks!!.track,
@@ -51,9 +33,10 @@ class Interactor(private val repo: MainRepository, private val retrofitService: 
         override fun getRefreshKey(state: PagingState<Int, Track>): Int? {
             TODO("Not yet implemented")
         }
-    }
     fun getTracksFromDB(): List<Audio> = repo.getAllFromDB()
-    }
+
+}
+
 
 
 
