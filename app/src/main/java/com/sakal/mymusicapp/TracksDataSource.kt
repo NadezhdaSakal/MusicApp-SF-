49cdae7eb4300e9cc50e6dafe83c77991dc1e338
NadezhdaSakal <79761587+NadezhdaSakal@users.noreply.github.com>
2022-06-24 15:13:41 +0300

    pagination

--- app/src/main/java/com/sakal/mymusicapp/TracksDataSource.kt
+++ app/src/main/java/com/sakal/mymusicapp/TracksDataSource.kt
@@ -8,25 +8,3 @@ import retrofit2.Call
 import retrofit2.Response
 import retrofit2.awaitResponse
 
-class TracksDataSource(private val api: LastFMApi) : PagingSource<Int, Track>() {
-
-        override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Track> {
-            return try {
-                val nextPageNumber = params.key ?: 0
-                val query: Call<TracksWrapper> = api.getTracks(50, 1)
-                val response: Response<TracksWrapper> = query.awaitResponse()
-
-                LoadResult.Page(
-                    data = response.body()?.tracks!!.track,
-                    prevKey = if (nextPageNumber > 0) nextPageNumber - 1 else null,
-                    nextKey = if (nextPageNumber < response.body()?.tracks?.attr?.totalPages!!.toInt()) nextPageNumber + 1 else null
-                )
-            } catch (e: Exception) {
-                LoadResult.Error(e)
-            }
-        }
-
-        override fun getRefreshKey(state: PagingState<Int, Track>): Int? {
-            TODO("Not yet implemented")
-        }
-    }
--- app/src/main/java/com/sakal/mymusicapp/domain/Interactor.kt
+++ app/src/main/java/com/sakal/mymusicapp/domain/Interactor.kt
@@ -1,13 +1,16 @@
 package com.sakal.mymusicapp.domain
 
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
 import com.sakal.mymusicapp.data.*
 import com.sakal.mymusicapp.data.entity.Audio
+import com.sakal.mymusicapp.data.entity.Track
 import com.sakal.mymusicapp.data.entity.TracksWrapper
 import com.sakal.mymusicapp.utils.Converter
 import com.sakal.mymusicapp.viewmodel.HomeFragmentViewModel
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.sakal.mymusicapp.viewmodel.TracksViewModel
+import retrofit2.*
+import java.io.IOException
 
 class Interactor(private val repo: MainRepository, private val retrofitService: LastFMApi) {
 
@@ -26,7 +29,30 @@ class Interactor(private val repo: MainRepository, private val retrofitService: 
                 }
             })
         }
-            fun getTracksFromDB(): List<Audio> = repo.getAllFromDB()
+
+    class TracksDataSource(private val api: LastFMApi) : PagingSource<Int, Track>() {
+
+        override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Track> {
+            return try {
+                val nextPageNumber = params.key ?: 0
+                val query: Call<TracksWrapper> = api.getTracks(50, 1)
+                val response: Response<TracksWrapper> = query.awaitResponse()
+
+                LoadResult.Page(
+                    data = response.body()?.tracks!!.track,
+                    prevKey = if (nextPageNumber > 0) nextPageNumber - 1 else null,
+                    nextKey = if (nextPageNumber < response.body()?.tracks?.attr?.totalPages!!.toInt()) nextPageNumber + 1 else null
+                )
+            } catch (e: Exception) {
+                LoadResult.Error(e)
+            }
+        }
+
+        override fun getRefreshKey(state: PagingState<Int, Track>): Int? {
+            TODO("Not yet implemented")
+        }
+    }
+    fun getTracksFromDB(): List<Audio> = repo.getAllFromDB()
     }
 
 
