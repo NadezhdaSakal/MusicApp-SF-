48a113e8c5444c86d8c50a7889a4d351262dedc3
NadezhdaSakal <79761587+NadezhdaSakal@users.noreply.github.com>
2022-06-26 21:44:55 +0300

    notification

--- app/src/main/java/com/sakal/mymusicapp/App.kt
+++ app/src/main/java/com/sakal/mymusicapp/App.kt
@@ -1,11 +1,16 @@
 package com.sakal.mymusicapp
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import com.sakal.mymusicapp.di.AppComponent
 import com.sakal.mymusicapp.di.DaggerAppComponent
 import com.sakal.mymusicapp.di.modules.RemoteModule
 import com.sakal.mymusicapp.di.modules.DomainModule
 import com.sakal.mymusicapp.di.modules.DatabaseModule
+import com.sakal.mymusicapp.view.notification.NotificationConstants.CHANNEL_ID
+
 
 
 
@@ -22,8 +27,19 @@ class App : Application() {
             .domainModule(DomainModule(this))
             .build()
 
-    }
 
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val name = "PlaylistChanel"
+        val descriptionText = "MyMusicApp Notification Chanel"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+
+        val mChannel = NotificationChannel(CHANNEL_ID, name, importance)
+        mChannel.description = descriptionText
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(mChannel)
+    }
+}
     companion object {
         lateinit var instance: App
             private set
---
+++ app/src/main/java/com/sakal/mymusicapp/view/notification/NotificationHelper.kt
@@ -0,0 +1,54 @@
+package com.sakal.mymusicapp.view.notification
+
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.sakal.mymusicapp.R
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+import com.sakal.mymusicapp.data.entity.Audio
+import com.sakal.mymusicapp.view.MainActivity
+
+
+object NotificationHelper {
+    fun createNotification(context: Context, audio: Audio) {
+        val mIntent = Intent(context, MainActivity::class.java)
+
+        val pendingIntent =
+            PendingIntent.getActivity(context, 0, mIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val builder = NotificationCompat.Builder(context!!, NotificationConstants.CHANNEL_ID).apply {
+            setSmallIcon(R.drawable.ic_baseline_latest)
+            setContentTitle("Новое в чарте")
+            setContentText(audio.track)
+            priority = NotificationCompat.PRIORITY_DEFAULT
+            setContentIntent(pendingIntent)
+            setAutoCancel(true)
+        }
+
+        val notificationManager = NotificationManagerCompat.from(context)
+
+        Glide.with(context)
+            .asBitmap()
+            .load(audio.image)
+            .into(object : CustomTarget<Bitmap>() {
+                override fun onLoadCleared(placeholder: Drawable?) {
+                }
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    builder.setStyle(NotificationCompat.BigPictureStyle().bigPicture(resource))
+
+                    notificationManager.notify(audio.id, builder.build())
+                }
+            })
+        notificationManager.notify(audio.id, builder.build())
+    }
+}
+
+
+
+
